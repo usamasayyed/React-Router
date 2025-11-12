0b279f27585f67200b089c068f348a322b9c5abb
Osama <Usayed614@gmail.com>
2025-11-12 18:38:36 +0530

    New

--- src/pages/Jobs.jsx
+++ src/pages/Jobs.jsx
@@ -1,18 +1,28 @@
-import React from 'react'
+import React from "react";
+import { Link, useLoaderData } from "react-router-dom";
 
 const Jobs = () => {
+  const Jobs = useLoaderData();
+
   return (
     <div>
-
+      {Jobs.map((Job) => {
+        return (
+          <Link key={Job.id}>
+            <p>{Job.id}</p>
+            <h4>{Job.title}</h4>
+            <p>{Job.location}</p>
+            <p>{Job.salary}</p>
+          </Link>
+        );
+      })}
     </div>
-  )
-}
+  );
+};
 
-export default Jobs
+export default Jobs;
 
-export  const JobsLoader = async()=>{
-
-const res = await fetch ("http://localhost:50000/jobs")
-return res.json();
-
-}
+export const JobsLoader = async () => {
+  const res = await fetch("http://localhost:3000/jobs");
+  return res.json();
+};
